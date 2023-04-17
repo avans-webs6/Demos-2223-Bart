9048a1c07e0d5483a444a05d4074cfee07330352
Bart Luijten <bfm.luijten@avans.nl>
2023-04-17 13:55:30 +0200

    Added components event-list, event-create and event-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventCreateComponent } from './event-create/event-create.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    EventCreateComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule
