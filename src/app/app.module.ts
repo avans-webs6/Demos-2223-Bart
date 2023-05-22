f0bdedf6872f016ade8f7b95eab5abd8a616aad4
Bart Luijten <bfm.luijten@avans.nl>
2023-05-22 09:50:46 +0200

    Added reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
@@ -8,18 +8,23 @@ import { EventCreateComponent } from './event-create/event-create.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
 import { EventService } from './event.service';
 import { AppRoutingModule } from './app-routing.module';
+import { SearchEventPipe } from './search-event.pipe';
+import { EventSearchComponent } from './event-search/event-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventListComponent,
     EventCreateComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    SearchEventPipe,
+    EventSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [EventService],
   bootstrap: [AppComponent]
--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../event.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-event-details',
@@ -10,10 +11,21 @@ import { EventService } from '../event.service';
 export class EventDetailsComponent implements OnInit {
 
   event: any;
+
+  eventForm: FormGroup = new FormGroup({});
   
-  constructor(private service: EventService, private route: ActivatedRoute) {
-    service.getEvent(this.route.snapshot.paramMap.get('id')).subscribe((event: any) => {
+  constructor(private service: EventService, private route: ActivatedRoute, private formBuilder: FormBuilder) {
+    let id = this.route.snapshot.paramMap.get('id');
+
+    service.getEvent(id).subscribe((event: any) => {
       this.event = event;
+
+      this.eventForm = this.formBuilder.group(event);
+
+      this.eventForm.valueChanges.subscribe(() => {
+        this.service.updateEvent(id, this.eventForm.value)
+
+      });
     });
    }
 
